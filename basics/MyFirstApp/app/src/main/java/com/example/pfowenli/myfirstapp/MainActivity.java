379f34452d637dece64e976321fe472dc4f4ab20
Owen P. F. Li <pfowenli@gmail.com>
2017-09-08 22:45:11 +0800

    create TextView and set ContentView to TextView

--- basics/MyFirstApp/app/src/main/java/com/example/pfowenli/myfirstapp/MainActivity.java
+++ basics/MyFirstApp/app/src/main/java/com/example/pfowenli/myfirstapp/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.pfowenli.myfirstapp;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        TextView textView = new TextView(this);
+        textView.setText("Owen P. F. LI");
+        textView.setTextColor(Color.BLUE);
+        textView.setTextSize(45);
+
+        setContentView(textView);
     }
 }
